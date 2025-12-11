2decf8ada1181c7ddb8538e9ad6b256e19dc49a1
Youssef Waleed Mohamed Ahmed <youssefwalid725@gmail.com>
2025-12-11 19:11:33 +0200

    add review feature

---
+++ backend/app/dto/reviews.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+class ReviewCreateRequest(BaseModel):
+    rating: float = Field(...,ge=1,le=5)
+    comment: str | None = Field(
+        default=None,
+        min_length=10,
+        max_length=1000,
+    )
+
+class ReviewResponse(BaseModel):
+    id: int
+    user_full_name: str
+    rating: float
+    comment: str | None
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+
+class DeleteResponse(BaseModel):
+    message: str
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from app.api.auth import router as auth_router
 from app.api.events import router as events_router
 from app.api.health import router as health_router
 from app.api.my_events import router as my_events_router
+from app.api.reviews import router as reviews_router
 from app.core import config
 from app.db.session import init_db
 from fastapi import FastAPI
@@ -10,7 +11,7 @@ from fastapi_pagination import add_pagination
 app = FastAPI()
 add_pagination(app)
 
-routers = [auth_router, events_router, my_events_router]
+routers = [auth_router, events_router, my_events_router, reviews_router]
 for router in routers:
     app.include_router(router, prefix=config.API_V1_STR)
 
